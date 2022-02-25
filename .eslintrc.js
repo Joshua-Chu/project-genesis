4a4c6b94a68c9e982d34495f0c664d5b2e9a9b30
Joshua-Chu <jpchu@up.edu.ph>
2022-02-25 22:06:42 +0800

    test the prettier with esliint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,46 @@
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: "module",
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
 
-	env: {
-		browser: true,
-		node: true,
-		es6: true,
-		jest: true,
-	},
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        jest: true,
+    },
 
-	settings: {
-		react: {
-			version: "detect",
-		},
-		"import/resolver": {
-			node: {
-				extensions: [".ts", ".tsx"],
-			},
-		},
-	},
+    settings: {
+        react: {
+            version: "detect",
+        },
+        "import/resolver": {
+            node: {
+                extensions: [".ts", ".tsx"],
+            },
+        },
+    },
 
-	plugins: ["@typescript-eslint"],
-	extends: [
-		"next/core-web-vitals",
-		"plugin:@typescript-eslint/recommended",
-		"airbnb",
-		"prettier",
-		"plugin:jsx-a11y/recommended",
-		"plugin:react/recommended",
-		"plugin:prettier/recommended",
-		"plugin:sonarjs/recommended",
-		"plugin:security/recommended",
-		"plugin:react-hooks/recommended",
-	],
+    plugins: ["@typescript-eslint", "plugin:prettier/recommended"],
+    extends: [
+        "next/core-web-vitals",
+        "plugin:@typescript-eslint/recommended",
+        "airbnb",
+        "prettier",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+        "plugin:sonarjs/recommended",
+        "plugin:security/recommended",
+        "plugin:react-hooks/recommended",
+    ],
 
-	rules: {},
+    rules: {
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+    },
 };
