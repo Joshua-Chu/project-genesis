7f3c425c7c28f6364b82f45104e133adb75e3567
Joshua-Chu <jpchu@up.edu.ph>
2022-02-26 21:22:05 +0800

    fix base and prefix configurations

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: isProd ? "/project-genesis/" : "",
-    basePath: isProd ? "/project-genesis/" : "",
+    assetPrefix: isProd ? "/project-genesis" : "",
+    basePath: isProd ? "/project-genesis" : "",
 };
 
 module.exports = nextConfig;
