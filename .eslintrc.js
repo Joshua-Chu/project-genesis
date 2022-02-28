5f9790f628a441fa1c900597150b5251b6ffd0cc
Joshua-Chu <jpchu@up.edu.ph>
2022-02-28 20:17:44 +0800

    add logo component

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,5 +59,13 @@ module.exports = {
                 tsx: "never",
             },
         ],
+        "jsx-a11y/anchor-is-valid": [
+            "error",
+            {
+                components: ["Link"],
+                specialLink: ["hrefLeft", "hrefRight"],
+                aspects: ["invalidHref", "preferButton"],
+            },
+        ],
     },
 };
