6da08f47dad792f3e38da53e3be4023c94b8ce5a
Joshua-Chu <jpchu@up.edu.ph>
2022-03-01 22:17:18 +0800

    add clip-path effect to contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,45 @@ import {
     Text,
     useColorMode,
 } from "@chakra-ui/react";
+import styled from "@emotion/styled";
 import Image from "next/image";
 import NextLink from "next/link";
 import Section from "../component/Section/Section";
+
+interface ContactFlexProps {
+    islightmode?: "light" | "dark";
+}
+
+const ContactFlex = styled<ContactFlexProps & typeof Flex>(Flex)`
+    position: relative;
+
+    .contact-overlay {
+        border-radius: 16px;
+        border: 1px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 49;
+        width: 100%;
+        height: 100%;
+        clip-path: circle(10% at 50% 50%);
+        transition: all 250ms ease-in;
+    }
+
+    &:hover .contact-overlay {
+        background: ${({ islightmode }) =>
+            islightmode === "light" ? "#2e3440" : "#88C0D0"};
+        clip-path: circle(75%);
+    }
+
+    &:hover * {
+        color: white;
+    }
+
+    * {
+        z-index: 50;
+    }
+`;
 
 function Home() {
     const { colorMode } = useColorMode();
@@ -184,34 +220,46 @@ function Home() {
             </div>
 
             <Section>
-                <Flex
-                    mb={500}
-                    maxW={790}
-                    direction="column"
-                    gap="16px"
-                    alignItems={["center", "center", "flex-end"]}
-                >
-                    <Text
-                        maxW={450}
-                        fontSize={[".8rem", "sm"]}
-                        textAlign={["center", "center", "left"]}
-                        as="p"
-                        color={colorMode === "light" ? "brand.200" : "white"}
+                <Flex mb={500} maxW={790} justifyContent="flex-end">
+                    <ContactFlex
+                        direction="column"
+                        gap="16px"
+                        p={8}
+                        rounded="lg"
+                        alignItems={["center", "center", "flex-end"]}
+                        className="contact-container"
+                        islightmode={colorMode}
                     >
-                        I once heard that - Ideas are only as good as the
-                        actions that follow the communication of those ideas.
-                    </Text>
-                    <Heading
-                        as="h4"
-                        color={colorMode === "light" ? "brand.200" : "white"}
-                        fontSize={["1.1rem", "40.5px"]}
-                    >
-                        Let&apos;s make it happen.
-                    </Heading>
+                        <div className="contact-overlay" />
+                        <Text
+                            className="contact-heading"
+                            maxW={450}
+                            fontSize={[".8rem", "sm"]}
+                            textAlign={["center", "center", "left"]}
+                            as="p"
+                            color={
+                                colorMode === "light" ? "brand.200" : "white"
+                            }
+                        >
+                            I once heard that - Ideas are only as good as the
+                            actions that follow the communication of those
+                            ideas.
+                        </Text>
+                        <Heading
+                            className="contact-subheading"
+                            as="h4"
+                            color={
+                                colorMode === "light" ? "brand.200" : "white"
+                            }
+                            fontSize={["1.1rem", "40.5px"]}
+                        >
+                            Let&apos;s make it happen.
+                        </Heading>
 
-                    <NextLink href="/" passHref>
-                        <Link>@heyjoshchu</Link>
-                    </NextLink>
+                        <NextLink href="/" passHref>
+                            <Link className="contact-link">@heyjoshchu</Link>
+                        </NextLink>
+                    </ContactFlex>
                 </Flex>
             </Section>
         </>
