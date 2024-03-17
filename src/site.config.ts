8814077333c98823298b691c81473007e797bb21
Joshua-Chu <jpchu@up.edu.ph>
2024-03-17 22:12:16 +0800

    init

---
+++ src/site.config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+  author: "Joshua Chu",
+  // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+  title: "im",
+  // Meta property used as the default description meta property
+  description: "An opinionated starter theme for Astro",
+  // HTML lang property, found in src/layouts/Base.astro L:18
+  lang: "en-GB",
+};
