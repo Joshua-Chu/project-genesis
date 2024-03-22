d4524a070c26835a49f4d25040b8e2d37b664581
Joshua-Chu <jpchu@up.edu.ph>
2024-03-22 12:16:09 +0800

    add more stylings and pagination

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,4 @@
-import { getCollection } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 
 /** Note: this function filters out draft posts based on the environment */
 export const getAllArticles = async () => {
@@ -6,3 +6,11 @@ export const getAllArticles = async () => {
     return import.meta.env.PROD ? data.draft !== true : true;
   });
 };
+
+export function sortMDByDate(articles: Array<CollectionEntry<"article">>) {
+  return articles.sort((a, b) => {
+    const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).valueOf();
+    const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).valueOf();
+    return bDate - aDate;
+  });
+}
