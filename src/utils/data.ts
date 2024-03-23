b014e7a5376da948895129b0b1feb48221089052
Joshua-Chu <jpchu@up.edu.ph>
2024-03-23 17:33:56 +0800

    add tags feature and route

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -14,3 +14,25 @@ export function sortMDByDate(articles: Array<CollectionEntry<"article">>) {
     return bDate - aDate;
   });
 }
+
+/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
+export function getAllTags(articles: Array<CollectionEntry<"article">>) {
+  return articles.flatMap((article) => [...article.data.tags]);
+}
+
+/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
+export function getUniqueTags(articles: Array<CollectionEntry<"article">>) {
+  return [...new Set(getAllTags(articles))];
+}
+
+/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
+export function getUniqueTagsWithCount(
+  articles: Array<CollectionEntry<"article">>,
+): Array<[string, number]> {
+  return [
+    ...getAllTags(articles).reduce(
+      (acc, t) => acc.set(t, (acc.get(t) || 0) + 1),
+      new Map<string, number>(),
+    ),
+  ].sort((a, b) => b[1] - a[1]);
+}
